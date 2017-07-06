c433c27e736a150580b50548074165a487d39bd7
Min Kim <hyeonmin@uw.edu>
2017-07-06 17:19:18 +0900

    implemented legend, text, checkbox, and radio buttons

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(function(){
-    const DEFAULTCOLUMN = '사망자수'
+    const DEFAULTCOLUMN = '사망률'
+    const DEFAULTGENDER = '남자'
     /**Initial setting */
     var margin = {
         top: 10,
@@ -30,28 +31,42 @@ $(function(){
                .attr('height', drawHeight)
                .attr('width', drawWidth)
 
+    var xAxisLabel = svg.append('g')
+                        .attr('transform', `translate(${margin.left}, ${drawHeight + margin.top})`)
+                        .attr('class', 'axis')
+
+    var yAxisLabel = svg.append('g')
+                        .attr('transform', `translate(${margin.left},${margin.top})`)
+
+
 
     d3.csv('./data/data.csv', function(error, allData){
-        // console.log(allData)
         var orderedData = [];
+        var selectedGender = [];
+        var selected = DEFAULTGENDER;
         var selectedColumn = DEFAULTCOLUMN
-        var xScale = d3.scaleBand()
-        var yScale = d3.scaleLinear()
+
+        var colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+
+        var yFormat = d3.format('.2s')
+        var xScale = d3.scaleBand();
+        var yScale = d3.scaleLinear();
         var xAxis = d3.axisBottom()
-        var yAxis = d3.axisLeft();
-                
-        function filterData(columnName){
-            orderedData = allData.sort((a,b)=>d3.descending(+a[columnName], +b[columnName]))
+        var yAxis = d3.axisLeft().tickFormat(yFormat);
+
+        function filterData(){
+            orderedData = allData.filter((d)=>(d['성별']) == selected)
+                                .sort((a,b)=>d3.descending(+a[selectedColumn], +b[selectedColumn]))
                                 .slice(0,10)
         }
         
         function setScale(){
             xScale.range([0, drawWidth])
                 .padding(0.1)
-                .domain(orderedData.map((d)=>d['군구']))
+                .domain(orderedData.map((d)=>(d['시'] + " " + d['군구'])))
 
             //set XScale
-            var yMax = orderedData[0][selectedColumn]
+            var yMax = orderedData[0][selectedColumn] * 1.1
             yScale.range([drawHeight, 0])
                 .domain([0, yMax]);
         }
@@ -59,33 +74,32 @@ $(function(){
         function setAxis(){
             xAxis.scale(xScale)
             yAxis.scale(yScale);
+
+            xAxisLabel.transition()
+                    .duration(1000)
+                    .call(xAxis)
+                    .selectAll('text')
+                    .attr('transform', 'rotate(-45)')
+                    .style('text-anchor', 'end');
+
+            yAxisLabel.transition().duration(1000).call(yAxis);
+
+
         }
         
         function draw(){
+            g.selectAll('rect').remove();
+            g.selectAll('text').remove();
             var bar = g.selectAll('rect')
-                    .data(allData)
-
-            var xAxisLabel = svg.append('g')
-                                .attr('transform', `translate(${margin.left}, ${drawHeight + margin.top})`)
-                                .attr('class', 'axis')
-                                .call(xAxis)
-                                .selectAll('text')
-                                .attr('transform', 'rotate(-45)')
-                                .style('text-anchor', 'end');
-
-            var yAxisLabel = svg.append('g')
-                                .attr('transform', `translate(${margin.left},${margin.top})`)
-                                .call(yAxis);
-            
-            var bar = g.selectAll('rect')
-                    .data(orderedData)
+                    .data(orderedData, (d)=>(d['시'] + " " + d['군구']))
 
             bar.enter()
             .append('rect')
             .merge(bar)
             .attr('class', 'bar')
-            .attr('x', (d)=>xScale(d['군구']))
+            .attr('x', (d)=>+xScale(d['시'] + " " + d['군구']))
             .attr('y', drawHeight)
+            .attr('fill', (d)=>colorScale(d['시']))
             .transition()
             .duration(1500)
             .delay((d, i)=> i*50)
@@ -93,18 +107,77 @@ $(function(){
             .attr('width', xScale.bandwidth())
             .attr('height', (d)=> drawHeight-yScale(d[selectedColumn]))
 
-            bar.exit()
-            .remove()
+            bar.enter()
+                .append("text")
+                .attr("x", (d)=>+xScale(d['시'] + " " + d['군구']) + 40)
+                .attr("y", 0)
+                .transition()
+                .duration(1500)
+                .delay((d, i)=> i*50)
+                .attr("y", (d)=> yScale(d[selectedColumn])-2)
+                .attr("text-anchor", "middle")
+                .text(function(d){
+                    return (d[selectedColumn] + (selectedColumn == '사망률' ? '%' : '명'));
+                    })
+    
+            bar.exit().remove();
+
+
+            $('.legendLinear').remove();
+
+            var legend = svg.append("g")
+                .attr("class", "legendLinear")
+                .attr("transform", `translate(${width*50/100},0)`);
+
+            var legendLinear = d3.legendColor()
+                .shapeWidth(40)
+                .orient('horizontal')
+                .scale(colorScale);
+
+            svg.select(".legendLinear").call(legendLinear);
         }
 
         function render(){
-            filterData(DEFAULTCOLUMN)
+            filterData()
             setScale();
             setAxis();
             draw();
         }
         render();
 
+        $("input[type=radio]").on('change', function() {
+            // Get value, determine if it is the sex or type controller
+            data = $(this).val();
+            switch(data){
+                case '1':
+                    selectedColumn = '사망자수'
+                    break;
+                case '2':
+                    selectedColumn = '사망률'
+                    break;
+            }
+            render()
+        })
 
+        $('input[type=checkbox]').on('click', ()=>{
+            selectedGender = []
+            $('input[type=checkbox]:checked').each(function() {
+                selectedGender.push($(this).val())
+            });
+            if(selectedGender.length == 2){
+                selected = '계'
+                render();
+            } else if (selectedGender.length == 1){
+                if(selectedGender[0]=='m'){
+                    selected = '남자'
+                } else {
+                    selected = '여자'
+                }
+                render();
+            } else {
+                g.selectAll('rect').remove();
+                g.selectAll('text').remove();
+            }
+        })
     })
 })
